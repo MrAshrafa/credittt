501e06fce0db3d9f22580291465503ff3cf72b2c
MrAshrafa <45470917+MrAshrafa@users.noreply.github.com>
2018-12-07 15:49:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const client2 = new Diacord.Client();
+const client2 = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
