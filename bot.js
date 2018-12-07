d73a5516d45ca17e89aa7d00ff49785b19a74d8d
MrAshrafa <45470917+MrAshrafa@users.noreply.github.com>
2018-12-07 15:25:22 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const client2 = new Diacord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+client.on('ready', () => {
+client.user.setGame(``,'https://www.twitch.tv/v5bz'); 
+}); 
+
+client.on('ready', async() => {
+var server = "514044187344109569"; // ايدي السررفر
+var channel = "514044229941723139";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , **')
+    },305);
+})
+
+
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
+
+
