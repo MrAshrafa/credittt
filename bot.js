ba307fe1eb8dcaa72c0bfa89aa6b001dee816e51
MrAshrafa <45470917+MrAshrafa@users.noreply.github.com>
2018-12-08 08:32:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,15 +29,3 @@ var channel = "520576978693259264";//ايدي الروم
 client.login(process.env.BOT_TOKEN);
 
 
-client2.on('ready', async() => {
-var server = "520576939690557470"; // ايدي السررفر
-var channel = "520577003355635740";//ايدي الروم
-    setInterval(()=>{
-    client2.guilds.get(server).channels.get(channel).send('**Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , **')
-    },305);
-})
-
-
-
-// THIS  MUST  BE  THIS  WAY
-client2.login(process.env.BOT_TOKEN2);
